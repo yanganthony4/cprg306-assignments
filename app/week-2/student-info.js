98b23ab4c4f6328ae11bad76cef6fd6c55b3cd44
yanganthony4 <yanganthony4@gmail.com>
2024-09-23 09:19:40 -0600

    Completed week 2 assignment
    Completed StudentInfo object, referenced it inside week-2 page and created home page

---
+++ app/week-2/student-info.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'next/link'
+
+const StudentInfo = () => {
+  return (
+    <div>
+        <h1>Anthony Yang</h1>
+        <Link href="https://github.com/yanganthony4/cprg306-assignments">Github Repository</Link>
+    </div>
+  )
+}
+
+export default StudentInfo
