27c8a8bb00dc72a8603aaf0813f01396649db6e0
yanganthony4 <yanganthony4@gmail.com>
2024-10-09 15:07:14 -0600

    Finished Week-5 Assignment
    Developed item input where end result would flash alert with user chosen values.

--- app/page.js
+++ app/page.js
@@ -9,6 +9,8 @@ export default function Home() {
       <Link href="/week-3">Week 3</Link>
       <br />
       <Link href="/week-4">Week 4</Link>
+      <br />
+      <Link href="/week-5">Week 5</Link>
     </div>
   );
 }
