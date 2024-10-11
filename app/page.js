e63e8faf282b73950df403ed30bf150dcf3d66b6
yanganthony4 <yanganthony4@gmail.com>
2024-10-11 13:58:01 -0600

    Finished week-6 assignment
    Re-created grocery list from week 3 but with sort by and group by functions

--- app/page.js
+++ app/page.js
@@ -11,6 +11,8 @@ export default function Home() {
       <Link href="/week-4">Week 4</Link>
       <br />
       <Link href="/week-5">Week 5</Link>
+      <br />
+      <Link href="/week-6">Week 6</Link>
     </div>
   );
 }
